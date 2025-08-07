b23682de0938cb1f19ef7945d4e36269ea8da274
Mohammad Ayub <mohammadayub1694@gmail.com>
2025-08-07 14:34:53 -0700

    Add files via upload

---
+++ first.js
@@ -0,0 +1,21 @@
+const product={
+    name : "pen",
+    rating : 5,
+    isDeal : true,
+    price : 50,
+};
+
+
+const profile = {
+    name : "malik ayub",
+    isFollow : true,
+    post : 31,
+    followers : "596k",
+    following : "35",
+
+}
+
+console.log(profile);
+
+
+
